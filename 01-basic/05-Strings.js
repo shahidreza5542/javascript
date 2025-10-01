45e73e36bd9f8af043cb17cbf72e4857629801c8
shahidreza <shahidreza660@gmail.com>
2025-10-01 14:06:08 +0530

    s

--- 01-basic/05-Strings.js
+++ 01-basic/05-Strings.js
@@ -1,4 +1,26 @@
 const Name = "shahid"
 const repoCount = 50
 
-console.log(Name + repoCount);
+// console.log(Name + repoCount);
+// console.log(`hello my name is ${Name} and my repo count is ${repoCount}`);
+
+const gamename = new String('shahid')
+// console.log(gamename[0]);
+// console.log(typeof(gamename)); // output object
+// console.log(gamename.length);
+// console.log(gamename.charAt(3));
+
+const newstring = gamename.substring(0, 4)
+console.log(newstring);
+
+const anotherString = gamename.slice(-5, 4)
+console.log(anotherString);
+
+const newstringOne = "    shahid.   "
+
+console.log(newstringOne);
+console.log(newstringOne.trim());
+
+const url = "https://shahid.com/shahid%20 reza"
+
+console.log(url.replaceAll(' ', '-'));
---
+++ 01-basic/06-math-number.js
@@ -0,0 +1,3 @@
+console.log(Math.E);
+console.log(Math.floor(Math.random() * 10000 + 1));
+
