4185aeac45261cf531339a58fa23e0d925d81f87
shahidreza <shahidreza660@gmail.com>
2025-10-06 13:39:23 +0530

    ds

--- 01-basic/07-scope.js
+++ 01-basic/07-scope.js
@@ -4,9 +4,23 @@ if (true) {
     let a = 10
     const b = 20
     var c = 30
-    console.log("INNER:",a);
+    // console.log("INNER:",a);
 }
 
-console.log(a);
-// console.log(b);
-console.log(c);
+// console.log(a);
+// // console.log(b);
+// console.log(c);
+
+function one(){
+    const username = "shahid"
+    
+    function two() {
+        const website = "youtube"
+        console.log(username);
+        
+    }
+    // console.log(website); // errors
+    two()
+}
+
+one()
