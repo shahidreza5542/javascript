3e777f38d47e1f3b98aaed447694b3ac1d4a746d
shahidreza <shahidreza660@gmail.com>
2025-10-06 14:04:51 +0530

    ds

--- 01-basic/08-arrowfunction.js
+++ 01-basic/08-arrowfunction.js
@@ -3,10 +3,34 @@ const user = {
     price: 999,
     welcomemassage: function () {
         console.log(`${this.username} welcome to website`);
+        // console.log(this);
     }
 
 }
 
-user.welcomemassage()
-user.username = "sam"
-user.welcomemassage()
+// user.welcomemassage()
+// user.username = "sam"
+// user.welcomemassage()
+
+// console.log(this);
+
+// function chai() {
+//     let username = "shahid"
+//     console.log(this.username);
+
+// }
+
+// chai()
+
+// const chai = function () {
+//     let username = "shahid"
+//     console.log(this.username);
+    
+// }
+
+const chai = () => {
+    let username = "shahid"
+    console.log(this.username);
+    
+}
+chai()
